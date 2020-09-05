53c037cfbc6a8678dbaaf9d53cd278fa2cdf6b69
Aral Roca Gomez <aral-rg@hotmail.com>
2020-09-05 10:47:36 +0200

    Implement resize (#25)

--- src/context/useRedraw.js
+++ src/context/useRedraw.js
@@ -1,9 +1,31 @@
 import { useEffect, useRef } from 'react'
 import { useDashboard } from '.'
 
+const cornerSize = 5
+
+/**
+ * Check if the point { x, y } is inside a box [bx, by, bx2, by2]
+ * 
+ * @param {number} x - point X to check
+ * @param {number} y - point Y to check
+ * @param {number} bx - startX of Box
+ * @param {number} by - startY of Box
+ * @param {number} bx2  - endX of Box
+ * @param {number} by2 - endY of Box
+ * @param {number} pad - Padding
+ */
+function isInside(x, y, bx, by, bx2, by2, pad = 0) {
+  return (
+    Math.min(bx - pad, bx2 + pad, x) !== x &&
+    Math.max(bx - pad, bx2 + pad, x) !== x &&
+    Math.min(by - pad, by2 + pad, y) !== y &&
+    Math.max(by - pad, by2 + pad, y) !== y
+  )
+}
+
 function arc(ctx, x, y) {
   ctx.beginPath()
-  ctx.arc(x, y, 5, 0, 2 * Math.PI)
+  ctx.arc(x, y, cornerSize, 0, 2 * Math.PI)
   ctx.closePath()
   ctx.fill()
 }
@@ -107,30 +129,48 @@ export function useSelectBox() {
   const redraw = useRedraw()
   const needsRedraw = useRef(false)
 
+  /**
+   * This function calculates if there where we are clicking corresponds to 
+   * a box. In addition, if where we click is one of the points to resize, 
+   * we return the opposite corner, this is useful to define the opposite 
+   * corner as the origin and where we are clicking as the end, so we can then
+   * resize the rectangles.
+   */
   function selectBox(e) {
     const { left, top } = canvasRef.current.getBoundingClientRect()
     const x = e.clientX - left
     const y = e.clientY - top
     const boxes = state.boxes || []
-    let selected
+    let selected, oppositeCorner
+    const padding = cornerSize + 2 / 2
 
-    boxes.forEach(([startX, startY, mouseX, mouseY], index) => {
-      if (
-        Math.min(startX, mouseX, x) !== x &&
-        Math.max(startX, mouseX, x) !== x &&
-        Math.min(startY, mouseY, y) !== y &&
-        Math.max(startY, mouseY, y) !== y
-      ) {
-        selected = index
+    for (let i = 0; i < boxes.length; i += 1) {
+      const [startX, startY, mouseX, mouseY] = boxes[i]
+      let c
+
+      if (isInside(x, y, startX, startY, startX, startY, padding)) {
+        c = oppositeCorner = [mouseX, mouseY]
       }
-    })
+      if (isInside(x, y, startX + (mouseX - startX), startY, startX + (mouseX - startX), startY, padding)) {
+        c = oppositeCorner = [startX, startY + (mouseY - startY)]
+      }
+      if (isInside(x, y, startX, startY + (mouseY - startY), startX, startY + (mouseY - startY), padding)) {
+        c = oppositeCorner = [startX + (mouseX - startX), startY]
+      }
+      if (isInside(x, y, mouseX, mouseY, mouseX, mouseY, padding)) {
+        c = oppositeCorner = [startX, startY]
+      }
+      if (c || isInside(x, y, startX, startY, mouseX, mouseY)) {
+        selected = i
+      }
+    }
 
     if (selected !== undefined) {
       needsRedraw.current = true
       dispatch({ type: 'select-box', data: selected })
     }
 
-    return selected
+    return { selected, oppositeCorner }
   }
 
   useEffect(() => {
